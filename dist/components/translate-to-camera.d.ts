f3725d75555afac871c416980c2198c345b1436e
neki-dev <dev@neki.guru>
2023-07-12 20:14:14 +0300

    Minor fixes

---
+++ dist/components/translate-to-camera.d.ts
@@ -0,0 +1,10 @@
+import React from 'react';
+type Props = {
+    position: {
+        x: number;
+        y: number;
+    };
+    children: React.ReactNode;
+};
+export declare const TranslateToCamera: React.FC<Props>;
+export {};
--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,3 +1,4 @@
+export * from './components';
 export * from './hooks';
 export * from './render';
 export * from './utils';
