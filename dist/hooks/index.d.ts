41593a83a5ff0f0d8d54a51eccdf186e0e607638
neki-dev <dev@neki.guru>
2023-07-14 01:53:45 +0300

    Fixed import

--- dist/hooks/index.d.ts
+++ dist/hooks/index.d.ts
@@ -2,4 +2,5 @@ export * from './use-game';
 export * from './use-current-scene';
 export * from './use-scene';
 export * from './use-scene-update';
+export * from './use-relative-position';
 export * from './use-relative-scale';
